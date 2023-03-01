ae1e019bf47aeec29e26637d6f978b8cb1ccb65f
AbishekPerera <abhishekperera77@gmail.com>
2023-03-01 20:30:02 +0530

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('listings');
+    return view('listings',[
+        'heading' => 'All Listings',
+        'listings' => Listing::all()
+    ]);
 });
+
