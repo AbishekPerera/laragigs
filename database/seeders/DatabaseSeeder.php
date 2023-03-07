922385d270d03e0a0fd80c55a51e290b372eacb1
AbishekPerera <abhishekperera77@gmail.com>
2023-03-07 19:45:53 +0530

    fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Listing::factory(15)->create();
+        // \App\Models\User::factory(10)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'jone@g.com'
+        ]);
+
+
+        Listing::factory(15)->create(
+            ['user_id' => $user->id]
+        );
     }
 }
